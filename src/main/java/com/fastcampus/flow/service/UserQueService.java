7577b088fc7337edba8e718a195488fd726e758b
jaemin <jjm7655@gmail.com>
2024-03-17 23:07:01 +0900

    shceduler init

--- src/main/java/com/fastcampus/flow/service/UserQueService.java
+++ src/main/java/com/fastcampus/flow/service/UserQueService.java
@@ -2,18 +2,30 @@ package com.fastcampus.flow.service;
 
 import com.fastcampus.flow.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.connection.zset.Tuple;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.core.ScanOptions;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuples;
 
 import java.time.Instant;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserQueService {
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
     private final String USER_QUEUE_WAIT_KEY = "users:queue:%s:wait";
+    private final String USER_QUEUE_WAIT_KEY_FOR_SCAN = "users:queue:*:wait";
     private final String USER_QUEUE_PROCEED_KEY = "users:queue:%s:proceed";
+
+    @Value("${scheduler.enabled}")
+    private Boolean scheduling = false;
+
 
     public Mono<Long> registerWaitQueue(final String queue, final Long userId) {
         var unixTimestamp = Instant.now().getEpochSecond();
@@ -39,6 +51,23 @@ public class UserQueService {
     public Mono<Long> getRank(final String queue, final Long userId) {
         return reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString())
                 .defaultIfEmpty(-1L)
-                .map(rank -> rank >= 0 ? rank + 1: rank);
+                .map(rank -> rank >= 0 ? rank + 1 : rank);
+    }
+
+    @Scheduled(initialDelay = 5000, fixedDelay = 10000)
+    public void scheduleAllowUser() {
+        if (!scheduling) {
+            log.info("pass scheduling");
+        }
+        log.info("called scheduling....");
+        var maxAllowedUserCount = 3L;
+        reactiveRedisTemplate.scan(ScanOptions.scanOptions()
+                        .match(USER_QUEUE_WAIT_KEY_FOR_SCAN)
+                        .count(100)
+                        .build())
+                .map(key -> key.split(":")[2])
+                .flatMap(queue -> allowUser(queue, maxAllowedUserCount).map(allowed -> Tuples.of(queue, allowed)))
+                .doOnNext(tuple -> log.info("Tried %d and Allowed %d members of %s queue".formatted(maxAllowedUserCount,tuple.getT2(), tuple.getT1())))
+                .subscribe();
     }
 }
